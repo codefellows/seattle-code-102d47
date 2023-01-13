fc992908cf52dbe188a652b3efd36db275e364be
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2023-01-13 13:58:54 -0800

    Review notes

---
+++ class-10/review/app.js
@@ -0,0 +1,47 @@
+// FUNCTION EXPRESSION
+
+var helloWorld = function(name) {
+  console.log('A new world awaits, ' + name);
+}
+
+// FUNCTION DECLARATION
+// function functionName(){
+  
+// }
+
+// let theName = "Ryan"
+// helloWorld(theName);
+
+
+// let theyAreWrong = true;
+
+// while(true){execute this code}
+
+// while(theyAreWrong){
+//   let response = prompt("Tell me the right answer...");
+//   console.log("User typed: " + response);
+//   if (response == "maybe"){
+//     theyAreWrong = false;
+//   }
+// }
+
+// for(initial value; condition to evaluate; increment/decrement){
+//    execute code
+// }
+//                       i -= 10    
+// for (let i = 100; i > 0; i = i - 10 ){
+//   console.log("First thing in the loop: " + i)
+//   i += 5;
+// }
+
+// Logical Operators
+
+// console.log((true || false) && (true !== false));
+//              TRUE         &&     TRUE
+
+let x = 5;
+let y = '5';
+
+// console.log(y != x);
+console.log((x > 0) && (y != x));
+//           True   &&   False
---
+++ class-10/review/review.md
@@ -0,0 +1,19 @@
+# Review Notes for Final Exam
+
+## Helpful Resources
+
+- [Replit.com](https://replit.com/~)
+- [Codepen.io](https://codepen.io/)
+- Google!
+  - How do I `<do the thing>` in `<what language>`?
+  - How do I add a border to an element in CSS?
+
+## What do you want to go over?
+
+- Terminology
+  - Functions
+- Loops
+- HTML
+  - Correct structure
+  - Quiz question 2
+- Quiz 7
